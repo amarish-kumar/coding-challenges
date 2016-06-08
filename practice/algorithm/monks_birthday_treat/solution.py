e871f4a1699411f311e5c94e66cf7099e1662f59
Amol Umrale <babaiscool@gmail.com>
2016-06-08 21:27:59 +0530

    Fixed logic.
    All test cases passing.

--- practice/algorithm/monks_birthday_treat/solution.py
+++ practice/algorithm/monks_birthday_treat/solution.py
@@ -1,37 +1,40 @@
-trees = []
 n, d = map(int, raw_input().split())
 
+edges = []
 for _ in range(0, d):
-	d1, d2 = map(int, raw_input().split())
+	v1, v2 = map(int, raw_input().split())
+	edges.append((v1, v2))
 
-	found = False
-	for t in trees:
-		if (d1 in t and d2 in t):
-			continue
-		if (d1 in t):
-			t.append(d2)
-			found = True
-		elif (d2 in t):
-			t.append(d1)
-			found = True
+if len(set([i for e in edges for i in e])) < n:		# some friends don't have a dependency, so, they won't be in the edge list
+	print 1
 
-	if not found:
-		trees.append([d1, d2])
+elif len([v2 for v1, v2 in edges if not v2 in [v1 for v1, _ in edges]]) > 0:	# friends only appearing on the right side of the edges don't have any
+	print 1									# dependency either
 
-	
+else:							# find smallest dependency tree
+	min_friends = 1001
 
-def intersect(t1, t2):
-	return any([i in t1 for i in t2])
+	def find_edges_starting_with(v):
+		return [e for e in edges if e[0] == v]
 
-def merge(t1, t2):
-	return list(set(t1 + t2))
+	def uniq(t):
+		return len(set([i for e in t for i in e]))			# count of unique vertices in a tree
 
+	for e in edges:		# form dependency tree for each edge
+		v1, v2 = e
+		tree = [e]
+		visit = [v1, v2]
 
-for i in range(0, len(trees) - 1):
-	for j in range(i+1, len(trees)):
-		if intersect(trees[i], trees[j]):
-			trees[i] = merge(trees[i], trees[j])
-			trees[j] = []
+		while len(visit) > 0:
+			deps = find_edges_starting_with(visit.pop())
+		
+			for dep in deps:
+				if not dep in tree:
+					tree.append(dep)
+					if not dep[1] in visit:
+						visit.append(dep[1])
 
-print min([len(t) for t in trees if len(t) > 0])
+		min_friends = min(min_friends, uniq(tree))
+		
+	print min_friends
 
