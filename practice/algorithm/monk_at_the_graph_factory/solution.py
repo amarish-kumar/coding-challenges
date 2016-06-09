e2e25dccc6b0ad2718f9c8c2335502a887d1c085
Amol Umrale <babaiscool@gmail.com>
2016-06-09 20:37:54 +0530

    Fixed issues.
    Reworked the logic.
    Now, we sort the list, in descending order, then, try to make the tree,
    by connecting (decrementing one) to the next vertex, thereby, possibly
    helping walk the tree if that vertex has degree more than one, and then,
    spending the remaining degrees by connecting to leaf vertices.
    
    All test cases passing.

--- practice/algorithm/monk_at_the_graph_factory/solution.py
+++ practice/algorithm/monk_at_the_graph_factory/solution.py
@@ -1,23 +1,28 @@
 c = input()
-vlist = map(int, raw_input().split())
+vlist = sorted(map(int, raw_input().split()), reverse=True)	# read and sort the degrees in descending order
 
 def walk(c, vlist):
-	for i in range(0, c):
+	for i in range(0, c-1):
 		v = vlist[i]
-		if (v == 1 and (i == 0 or any([k == 1 for k in vlist[0:i]])) and not (i == (c-2) and vlist[c-1] == 1)) or v == 0:
-			continue
 
-		for j in range(0, c):
-			if j != i and vlist[j] > 0:
-				vlist[j] -= 1
+		if v == 0:
+			return	
+
+		if vlist[i+1] > 0:	# possibly connect to the vertex which will in turn connect to more vertices, thus, helping us walk the tree
+			vlist[i+1] -= 1
+			v -= 1
+
+		for j in range(i+2, c):		# consume leaf vertices
+			if vlist[j] == 1:
+				vlist[j] = 0
 				v -= 1
 			if v == 0:
 				break
+	
 		vlist[i] = v
 
 walk(c, vlist)
 
-if any([i > 0 for i in vlist]):
-	print 'No'
-else:
-	print 'Yes'
+if max(vlist) == 0: print 'Yes'
+else: print 'No'
+
